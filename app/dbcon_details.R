bd045e2a5ef6ff79b1ad268158fbca6b43d0006e
attilatoth86 <attila.toth86@gmail.com>
2016-09-25 17:03:25 +0200

    Migration to PostgreSQL
    removing MySQL related db information

--- app/dbcon_details.R
+++ app/dbcon_details.R
@@ -1,16 +1,6 @@
-dbuser <- "root"
-dbpw <- "bitnami"
-dburl <- "172.16.127.129"
-dbname <- "finapp"
 
 psqldbuser <- "postgres"
 psqldbpw <- "bitnami"
 psqldburl <- "172.16.127.130"
 psqldbport <- 5432
 psqldbname <- "finapp"
-
-# save(dbuser,
-#      dbpw,
-#      dburl,
-#      dbname,
-#      file="dbcon_variables.RDS")
