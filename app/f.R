b0a058b2beaacc178efc7c947d301c226a829652
attilatoth86 <attila.toth86@gmail.com>
2016-09-25 17:02:05 +0200

    Migration to PostgreSQL
    removing MySQL related functions

--- app/f.R
+++ app/f.R
@@ -1,4 +1,5 @@
 library("RPostgreSQL")
+
 psqlQuery <- function(statement){
     drv <- dbDriver("PostgreSQL")
     source("dbcon_details.R", local = T)
@@ -6,64 +7,16 @@ psqlQuery <- function(statement){
                          host = psqldburl, port = psqldbport,
                          user = psqldbuser, password = psqldbpw)
     data <- dbGetQuery(psqlcon, statement)
+    RPostgreSQL::dbDisconnect(psqlcon)
     data
 }
+
 psqlInsert <- function(df, tablename){
     drv <- dbDriver("PostgreSQL")
     source("dbcon_details.R", local = T)
     psqlcon <- dbConnect(drv, dbname = psqldbname,
                          host = psqldburl, port = psqldbport,
                          user = psqldbuser, password = psqldbpw)
-    dbWriteTable(psqlcon, tablename, 
-                 value = df, append = TRUE, row.names = FALSE)
-}
-
-
-library("RMySQL")
-dbquery <- function(querytext){
-            # import db connection details
-            source("dbcon_details.R", local = T)
-            # establish db connection
-            dbcon <- dbConnect(MySQL(),
-                               user=dbuser, 
-                               password=dbpw,
-                               dbname=dbname, 
-                               host=dburl)
-            # query
-            rs <- dbSendQuery(dbcon, querytext)
-            data <- fetch(rs, n = -1)
-            huh <- dbHasCompleted(rs)
-            dbClearResult(rs)
-            dbDisconnect(dbcon)
-
-            data
-}
-
-dbinsert <- function(df,ld_tablename){
-    # import db connection details
-    source("dbcon_details.R")
-    # establish db connection
-    dbcon <- dbConnect(MySQL(),
-                       user=dbuser, 
-                       password=dbpw,
-                       dbname=dbname, 
-                       host=dburl)
-    # Truncate table beforehand
-    truncateStatement <- sprintf("TRUNCATE TABLE finapp.%s",ld_tablename)
-    dbSendQuery(dbcon, truncateStatement)
-    # insert into database
-    dbWriteTable(dbcon, name=ld_tablename, value=df, row.names=F, append = T)
-}
-
-dbUIinsert <- function(df,tablename){
-    # import db connection details
-    source("dbcon_details.R")
-    # establish db connection
-    dbcon <- dbConnect(MySQL(),
-                       user=dbuser, 
-                       password=dbpw,
-                       dbname=dbname, 
-                       host=dburl)
-    # insert into database
-    dbWriteTable(dbcon, name=tablename, value=df, row.names=F, append = T)
+    RPostgreSQL::dbWriteTable(psqlcon, tablename, value = df, append = T, row.names = F)
+    RPostgreSQL::dbDisconnect(psqlcon)
 }
