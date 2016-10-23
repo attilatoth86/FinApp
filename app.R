fe208bdace4170268a3dc48f1fcbb5aadde11a5b
attilatoth86 <attila.toth86@gmail.com>
2016-10-23 13:56:29 +0200

    folder structure changes

--- app.R
+++ app.R
@@ -17,6 +17,52 @@ library(XML)
 
 source("f.R")
 
+# init populate FX rates --------------------------------------------------
+
+import_fx_df <- psqlQuery("SELECT 
+                           MIN(a.date_open) date_open,
+                           c.iso_code
+                           FROM 
+                           account a, 
+                           currency c
+                           WHERE a.currency_id=c.id
+                           AND c.iso_code!='HUF'
+                           GROUP BY c.iso_code")$result
+
+psqlQuery("TRUNCATE TABLE ld_rate_value")$result
+
+for(i in 1:length(import_fx_df$iso_code)) {
+    iso_code <- import_fx_df$iso_code[i]
+    
+    fx_firstDateToImport <- psqlQuery(sprintf("SELECT COALESCE(MAX(value_date)+1,'%s') value_date 
+                                              FROM rate_value_fx_vw t WHERE t.rate_name='%s'",
+                                              import_fx_df$date_open[i],
+                                              paste0("HUF",iso_code)))$result[1,1]
+    
+    retrieve_url <- sprintf("https://www.mnb.hu/en/arfolyam-tablazat?deviza=rbCurrencySelect&devizaSelected=%s&datefrom=%s&datetill=%s&order=1", 
+                            iso_code,
+                            fx_firstDateToImport,
+                            Sys.Date())
+    web_import <- getURL(retrieve_url)
+    df_import <- readHTMLTable(web_import, trim = T, header = F, as.data.frame = T, stringsAsFactors=F)[[1]]
+    if(is.null(df_import)==F){
+        df_import[,1] <- as.Date(df_import[,1], format = "%d %B %Y")
+        df_import[,2] <- as.numeric(df_import[,2])
+        df_import[,3] <- sprintf("HUF%s",import_fx_df$iso_code[i])
+        colnames(df_import) <- c("value_date","value","rate_name")
+        psqlInsert(df_import, "ld_rate_value")    
+    }
+}
+
+psqlQuery("INSERT INTO rate_value (rate_id,value_date,value)
+          SELECT
+          r.id::int,
+          to_date(ldrv.value_date,'yyyy-mm-dd'),
+          ldrv.value::float
+          FROM
+          ld_rate_value ldrv
+          LEFT OUTER JOIN rate r ON ldrv.rate_name=r.rate_name")$result
+
 # init variables -----------------------------------------------
 
 fund_inv_ov_perioddays <- psqlQuery("SELECT
